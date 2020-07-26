e896ac59f64eb3cc3fd3b38365b175d9a5eb0731
shoaib soomro <64408582+soomroshoaib@users.noreply.github.com>
2020-07-26 03:33:27 -0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,73 @@
+var questions = [{
+    "question": "Saylani Mass IT training manager ?",
+    "option1": "Ghous Ahmed",
+    "option2": "Basit Ali",
+    "option3": "Muhammad Ali Mughal",
+    "option4": "Wajid Ullah",
+    "answer": "3"
+}, {
+    "question": "the major language of world wide web is ?",
+    "option1": "html",
+    "option2": "php",
+    "option3": "css",
+    "option4": "java",
+    "answer": "1"
+}, {
+    "question": "URL is abbrevviation of  ?",
+    "option1": "Universal Resources Locator",
+    "option2": "Uniform Resources Locator",
+    "option3": "Univer Resources Locator",
+    "option4": "none",
+    "answer": "2"
+},{
+    "question": "The network in which we connect each network node to a central device (hub) is called?",
+    "option1": "Bus Network topology",
+    "option2": "Star Network topology ",
+    "option3": "  Rining topology",
+    "option4": "none",
+    "answer": "2"
+}, {
+    "question": "FTP is an abbreviation of ?",
+    "option1": " File Transfer Position ",
+    "option2": " File Transfer protection",
+    "option3": " File Transfer protocol",
+    "option4": " File Transfer possibility",
+    "answer": "3"
+}, {
+    "question": "A virus that replicaties itself is called",
+    "option1": "bug",
+    "option2": "worm",
+    "option3": " bomb",
+    "option4": "hoax",
+    "answer": "1"
+}, {
+    "question": "File compression utilties can reduce the file size by",
+    "option1": " 10%",
+    "option2": " 60%",
+    "option3": " 80% ",
+    "option4": "100%",
+    "answer": "3"
+}, {
+    "question": " In C language , .exe file is produced by ?",
+    "option1": "Linker ",
+    "option2": "loader ",
+    "option3": " compiler ",
+    "option4": "interpreter",
+    "answer": "1"
+}, {
+    "question": "The number of Bytes used by char data type in C is ?",
+    "option1": "1",
+    "option2": "2",
+    "option3": "4",
+    "option4": "3",
+    "answer": "1"
+}, {
+    "question": "What does the prefix pico means ? ",
+    "option1": "Hunderdth",
+    "option2": "Millionth",
+    "option3": "Billionth",
+    "option4": "Trillionth",
+    "answer": "4"
+}
+
+]
